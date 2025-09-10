e19fd9e795e892ea5c9c8cfd595ca0b89b33dff9
Rohan-Panchal-Wintree <rohanpanchal@wintreetech.com>
2025-09-10 12:44:18 +0530

    updated the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  // plugins: [react(), mode === "production" && componentTagger()].filter(
+  //   Boolean
+  // ),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
