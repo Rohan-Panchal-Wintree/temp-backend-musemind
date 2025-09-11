6a39ae191ffccdace7b23877dc1366494b7dffae
Rohan-Panchal-Wintree <rohanpanchal@wintreetech.com>
2025-09-11 12:52:47 +0530

    added a consol;e in the pricing page to check if it works on reload

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -33,6 +33,7 @@ const MAX_PRICE = 500;
 // payment merchantTransactionId=01234" \,paymentType=DB" \, "currency=EUR" \
 
 const Pricing = () => {
+  console.log("Pricing page mounted");
   const [showPayment, setShowPayment] = useState(false);
   const [selectedPackage, setSelectedPackage] = useState<{
     credits: number;
